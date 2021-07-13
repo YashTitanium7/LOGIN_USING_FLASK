910de0eff4da1ef11d9444a21ac18762266b0e7c
YashTitanium7 <yash.titanium@gmail.com>
2021-07-13 08:44:14 +0530

    UI completed

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, app
+from flask import Flask, app, request, redirect
 from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///loginDatabase.db"
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-  return render_template('index.html')
+  return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+    password = hash(password)
+    return render_template('testing.html', email=email, password=password)
+  return render_template('login.html')
+
+@app.route("/reg", methods=['POST', 'GET'])
+def reg():
+  if request.method == 'POST':
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    password = hash(password)
+    return render_template('testing.html', name=name, email=email, password=password)
+  return render_template('register.html')
 
 if __name__ == '__main__':
   app.run(debug=True, port="5500")
