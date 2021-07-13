40c31ee5147e2a46d09c2964984c8a73c9335b3a
YashTitanium7 <yash.titanium@gmail.com>
2021-07-13 15:54:14 +0530

    Database testing successful

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///loginDatabase.db"
 db = SQLAlchemy(app)
 
+class Register(db.Model):
+  sno = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+  email = db.Column(db.String(200), nullable=False)
+  password = db.Column(db.String(200), nullable=False)
+
+  def __repr__(self) -> str:
+    return f"{self.sno} - {self.name}"
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
   return redirect('/login')
@@ -26,6 +35,9 @@ def reg():
     email = request.form.get('email')
     password = request.form.get('password')
     password = hash(password)
+    ACs = Register(name=name, email=email, password=password)
+    db.session.add(ACs)
+    db.session.commit()
     return render_template('testing.html', name=name, email=email, password=password)
   return render_template('register.html')
 
